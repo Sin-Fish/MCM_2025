796090267ff7b63ca9bad00c284f27b91bf93e23
Aeolyn <null404null707@163.com>
2025-09-05 08:59:57 +0800

    refactor(data): 重命名数据文件并添加数据清洗工具
    - 将数据文件重命名为 data.xlsx
    - 新增 data_cleaning.py 文件，实现数据导入、列扫描修复和数据精度限制功能

---
+++ data/util/data_cleaning.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import os
+
+class data_cleaner:
+    def __init__(self, config):
+        self.config = config
+        self.data = data_cleaner.import_data(config["file_path"])
+        
+
+    
+    def scan_and_fix_column(self,col):
+        """
+        param:
+        col: 列号
+        """
+        
+       
+        self.data.iloc[:, col].fillna(0, inplace=True)
+
+    
+    def limit_precision(self,col:int, precision:int):
+        """
+        数据数度限制
+        param:
+        col: 列号
+        precision: 保留的小数位数
+        
+        """
+        
+        self.data.iloc[:, col] = self.data.iloc[:, col].round(precision)
+
+    def import_data(file_path):
+        """
+        数据导入
+        """
+        try:
+            print(f"正在导入数据文件 {file_path}")
+            data = pd.read_excel(file_path)
+        except Exception as e:
+            print(f"导入数据文件 {file_path} 失败：{e}")
+            return None
+        print(f"数据文件 {file_path} 导入成功")
+        return data
+    def save_data(self, save_path):
+        """
+        数据保存
+        """
+       
+        directory = os.path.dirname(save_path)
+       
+        if directory and not os.path.exists(directory):
+            os.makedirs(directory)
+        self.data.to_excel(save_path, index=False)
+
+if __name__ == "__main__":
+    config = {
+        "file_path": "../data/data.xlsx",  
+        "save_path": "../result/cleaned_data.xlsx",  
+    }
+
+    cleaner = data_cleaner(config)
+    cleaner.save_data(config["save_path"])
